3803be0767966590352f24b8a2617e8128c02ce3
jake@seahag.onmicrosoft.com <jake@seahag.onmicrosoft.com>
2023-06-13 17:11:13 -0500

    hh

--- app.js
+++ app.js
@@ -1,26 +1,75 @@
-var metronome = new Metronome();
+var metronome = new Metronome(120, 4);
+
+window.metronome = metronome
+
 var tempo = document.getElementById('tempo');
+
 tempo.textContent = metronome.tempo;
 
 var playPauseIcon = document.getElementById('play-pause-icon');
 
 var playButton = document.getElementById('play-button');
+
 playButton.addEventListener('click', function() {
-    metronome.startStop();
+  metronome.startStop();
 
-    if (metronome.isRunning) {
-        playPauseIcon.className = 'pause';
-    }
-    else {
-        playPauseIcon.className = 'play';
-    }
-
+  if (metronome.isRunning) {
+    playPauseIcon.className = 'pause';
+  }
+  else {
+    playPauseIcon.className = 'play';
+  }
 });
 
 var tempoChangeButtons = document.getElementsByClassName('tempo-change');
+
 for (var i = 0; i < tempoChangeButtons.length; i++) {
-    tempoChangeButtons[i].addEventListener('click', function() {
-        metronome.tempo += parseInt(this.dataset.change);
-        tempo.textContent = metronome.tempo;
-    });
+  tempoChangeButtons[i].addEventListener('click', function() {
+    metronome.tempo += parseInt(this.dataset.change);
+    tempo.textContent = metronome.tempo;
+  });
 }
+
+const barDisplay = document.querySelector('#time-display-bars');
+const beatDisplay = document.querySelector('#time-display-beats');
+const secondsDisplay = document.querySelector('#time-display-seconds');
+
+
+const beatStepper = document.querySelector('#beat-stepper');
+let currentBeatStep = null;
+let lastBeat = null;
+
+const updateUI = (state) => {
+  const { bar, beat, time, nextNote } = state
+  let adjustedTime = time.toFixed(2);
+
+  barDisplay.textContent = bar;
+  beatDisplay.textContent = beat;
+  secondsDisplay.textContent = adjustedTime // time.toFixed(2);
+
+  if (currentBeatStep instanceof HTMLElement) {
+    currentBeatStep.classList.remove('active');
+  }
+
+  currentBeatStep = beatStepper.querySelector('#b' + beat);
+
+  currentBeatStep.classList.add('active');
+};
+
+const startLoop = () => {
+  let beat;
+  
+  while (metronome.beatQueue.length && metronome.beatQueue[0].time < metronome.currentTime) {
+    beat = metronome.beatQueue[0];
+    metronome.beatQueue.shift(); // Remove note from queue
+  }
+
+  if (beat && lastBeat !== beat) {
+    updateUI(beat)
+  }
+  
+  requestAnimationFrame(startLoop)
+};
+
+
+startLoop();
--- metronome.js
+++ metronome.js
@@ -1,92 +1,139 @@
-class Metronome
-{
-    constructor(tempo = 120)
-    {
-        this.audioContext = null;
-        this.notesInQueue = [];         // notes that have been put into the web audio and may or may not have been played yet {note, time}
-        this.currentBeatInBar = 0;
-        this.beatsPerBar = 4;
-        this.tempo = tempo;
-        this.lookahead = 25;          // How frequently to call scheduling function (in milliseconds)
-        this.scheduleAheadTime = 0.1;   // How far ahead to schedule audio (sec)
-        this.nextNoteTime = 0.0;     // when the next note is due
-        this.isRunning = false;
-        this.intervalID = null;
+class Metronome extends EventTarget {
+  audioContext = null;
+  beatQueue = []; // notes that have been put into the web audio and may or may not have been played yet {note, time}
+  currentBeatInBar = 0;
+  currentBar = 0;
+  beatsPerBar = 4;
+  lookahead = 25; // How frequently to call scheduling function (in milliseconds)
+  scheduleAheadTime = 0.1; // How far ahead to schedule audio (sec)
+  nextNoteTime = 0.0; // when the next note is due
+  isRunning = false;
+  intervalID = null;
+  tempo = 120;
+  beatsPlayed = 0
+  latencyCompensation = 0.08
+
+  constructor(tempo = 120, beatsPerBar = 4, latencyCompensation) {
+    super();
+
+    this.beatsPerBar = beatsPerBar;
+    this.tempo = tempo;
+    this.latencyCompensation = latencyCompensation || this.latencyCompensation;
+  }
+
+  get secondsPerBeat() {
+    return 60.0 / this.tempo;
+  }
+
+  get currentTime() {
+    return this.audioContext.currentTime;
+  }
+
+  nextNote() { // Advance current note and time by a quarter note (crotchet if you're posh)
+
+    // Add beat length to last beat time
+    this.nextNoteTime += this.secondsPerBeat;
+
+    // Advance the beat number, wrap to zero
+    this.currentBeatInBar++;
+    this.beatsPlayed++;
+
+    // console.log('this.beatsPlayed', this.beatsPlayed);
+
+    if (this.currentBeatInBar == this.beatsPerBar) {
+      this.currentBar++;
+      this.currentBeatInBar = 0;
+    }
+  }
+
+  scheduleNote(beatNumber, time) {
+    const osc = this.audioContext.createOscillator();
+
+    const envelope = this.audioContext.createGain();
+
+    // push the note on the queue, even if we're not playing.
+    // this.beatQueue.push({
+    //   note: beatNumber,
+    //   time,
+    // });
+
+    this.beatQueue.push({
+      bar: this.currentBar,
+      beat: this.currentBeatInBar,
+      time: this.currentTime,
+      nextNote: this.nextNoteTime
+    });
+    // console.log('[[ scheduleNote INSIDE LOOP ]]', this.beatQueue)
+
+    envelope.gain.exponentialRampToValueAtTime(1, time + 0.001);
+    envelope.gain.exponentialRampToValueAtTime(0.001, time + 0.02);
+
+    osc.frequency.value = (beatNumber % this.beatsPerBar == 0) ? 1000 : 800;
+
+    osc.connect(envelope);
+
+    envelope.connect(this.audioContext.destination);
+
+    osc.start(time);
+    osc.stop(time + 0.03);
+  }
+
+  scheduler() {
+    // while there are notes that will need 
+    // to play before the next interval, 
+    // schedule them and advance the pointer.
+
+    while (this.nextNoteTime < this.currentTime + this.scheduleAheadTime) {
+      this.scheduleNote(this.currentBeatInBar, this.nextNoteTime);
+      this.nextNote();
     }
 
-    nextNote()
-    {
-        // Advance current note and time by a quarter note (crotchet if you're posh)
-        var secondsPerBeat = 60.0 / this.tempo; // Notice this picks up the CURRENT tempo value to calculate beat length.
-        this.nextNoteTime += secondsPerBeat; // Add beat length to last beat time
-    
-        this.currentBeatInBar++;    // Advance the beat number, wrap to zero
-        if (this.currentBeatInBar == this.beatsPerBar) {
-            this.currentBeatInBar = 0;
+    this.dispatchEvent(
+      new CustomEvent('metronome:tick', {
+        bubbles: true,
+        detail: {
+          bar: this.currentBar,
+          beat: this.currentBeatInBar,
+          time: this.currentTime,
+          nextNote: this.nextNoteTime
         }
+      })
+    );
+
+    this.intervalID = setTimeout(this.scheduler.bind(this), this.lookahead);
+  }
+
+  start() {
+    if (this.isRunning) return;
+    this.audioContext = new(window.AudioContext || window.webkitAudioContext)();
+
+    if (this.audioContext == null) {}
+
+    this.isRunning = true;
+
+    this.nextNoteTime = 0.0;
+    this.currentBar = 0;
+    this.currentBeatInBar = 0;
+    this.nextNoteTime = this.currentTime + this.latencyCompensation //0.08;
+
+    this.scheduler();
+  }
+
+  stop() {
+    this.isRunning = false;
+
+    clearInterval(this.intervalID);
+
+    this.intervalID = null;
+  }
+
+  startStop() {
+    if (this.isRunning) {
+      this.stop();
     }
 
-    scheduleNote(beatNumber, time)
-    {
-        // push the note on the queue, even if we're not playing.
-        this.notesInQueue.push({ note: beatNumber, time: time });
-    
-        // create an oscillator
-        const osc = this.audioContext.createOscillator();
-        const envelope = this.audioContext.createGain();
-        
-        osc.frequency.value = (beatNumber % this.beatsPerBar == 0) ? 1000 : 800;
-        envelope.gain.value = 1;
-        envelope.gain.exponentialRampToValueAtTime(1, time + 0.001);
-        envelope.gain.exponentialRampToValueAtTime(0.001, time + 0.02);
-
-        osc.connect(envelope);
-        envelope.connect(this.audioContext.destination);
-    
-        osc.start(time);
-        osc.stop(time + 0.03);
+    else {
+      this.start();
     }
-
-    scheduler()
-    {
-        // while there are notes that will need to play before the next interval, schedule them and advance the pointer.
-        while (this.nextNoteTime < this.audioContext.currentTime + this.scheduleAheadTime ) {
-            this.scheduleNote(this.currentBeatInBar, this.nextNoteTime);
-            this.nextNote();
-        }
-    }
-
-    start()
-    {
-        if (this.isRunning) return;
-
-        if (this.audioContext == null)
-        {
-            this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        }
-
-        this.isRunning = true;
-
-        this.currentBeatInBar = 0;
-        this.nextNoteTime = this.audioContext.currentTime + 0.05;
-
-        this.intervalID = setInterval(() => this.scheduler(), this.lookahead);
-    }
-
-    stop()
-    {
-        this.isRunning = false;
-
-        clearInterval(this.intervalID);
-    }
-
-    startStop()
-    {
-        if (this.isRunning) {
-            this.stop();
-        }
-        else {
-            this.start();
-        }
-    }
+  }
 }
